dac062f92aafb6ba00ee67cceffd073a6a0b241d
SakshiKekre <sakshi.kekre@gmail.com>
2024-05-05 21:39:52 -0700

    preliminary API Integration for user data

--- frontend/tradebot/src/pages/ProfilePage.js
+++ frontend/tradebot/src/pages/ProfilePage.js
@@ -1,19 +1,40 @@
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Sidebar from '../components/Sidebar';
+import axios from 'axios';
 
 const ProfilePage = () => {
   // Placeholder user data
   const [userData, setUserData] = useState({
-    firstName: 'Trader',
-    lastName: 'Moe',
-    email: 'john.doe@example.com',
-    tradeAPI: 'some yahoo trading API',
+    name: null,
+    user_api: null,
+    user_risk_cap: null,
+    user_market_cap: null,
+    username: null,
     // Add more fields as needed
   });
 
   // State for form inputs
   const [formValues, setFormValues] = useState(userData);
+
+  // Function to fetch user info from the server
+  const getUserInfo = async () => {
+    try {
+      const response = await axios.get('http://127.0.0.1:5000/get_userinfo');
+      const responseData = response.data[0];
+
+      console.log("data:", responseData);
+      // Update user data and form values with fetched data
+      setUserData(responseData);
+      setFormValues(responseData);
+    } catch (error) {
+      console.error('Error fetching user info:', error);
+    }
+  };
+
+  // Call getUserInfo function when the component mounts
+  useEffect(() => {
+    getUserInfo();
+  }, []);
 
   // Handle form input changes
   const handleInputChange = (e) => {
@@ -22,9 +43,27 @@ const ProfilePage = () => {
   };
 
   // Handle form submission
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     // Update user data in the backend or perform other actions
+
+    try {
+      // Make a POST request to the backend API endpoint
+      const response = await axios.post('http://127.0.0.1:5000/user_post', formValues);
+  
+      // Check if the request was successful
+      if (response.status === 200) {
+        console.log('User data submitted successfully:', response.data);
+        // Optionally update the state to reflect changes
+        setUserData(formValues);
+      } else {
+        console.error('Error submitting user data:', response.data.message);
+      }
+    } catch (error) {
+      console.error('Error submitting user data:', error.message);
+    }
+
+
     console.log('Form submitted with values:', formValues);
     // Optionally update the state to reflect changes
     setUserData(formValues);
@@ -32,102 +71,91 @@ const ProfilePage = () => {
 
   return (
     <div className="background container">
-
       <div className="sidebar">
-        <Sidebar></Sidebar>
+        <Sidebar />
       </div>
 
-      <div className='main'>
-        <div class="profile-container">
+      <div className="main">
+        <div className="profile-container">
           <h1>User Profile</h1>
           <h2>Identification Information</h2>
-          <form onSubmit={handleSubmit} class="profile-info form-container">
-            <div class="form-field">
+          <form onSubmit={handleSubmit} className="profile-info form-container">
+            <div className="form-field">
               <label htmlFor="firstName">First Name:</label>
               <input
                 type="text"
                 id="firstName"
-                name="firstName"
-                value={formValues.firstName}
+                name="name"
+                value={formValues.name}
                 onChange={handleInputChange}
               />
             </div>
-            <div class="form-field">
-              <label htmlFor="lastName">Last Name:</label>
+            <div className="form-field">
+              <label htmlFor="user_market_cap">Market Cap:</label>
               <input
                 type="text"
-                id="lastName"
-                name="lastName"
-                value={formValues.lastName}
+                id="user_market_cap"
+                name="user_market_cap"
+                value={formValues.user_market_cap}
                 onChange={handleInputChange}
               />
             </div>
-            <div class="form-field">
+            <div className="form-field">
               <label htmlFor="email">Email:</label>
               <input
                 type="email"
                 id="email"
-                name="email"
-                value={formValues.email}
+                name="username"
+                value={formValues.username}
                 onChange={handleInputChange}
               />
             </div>
-            <div class="form-field">
+            <div className="form-field">
               <label htmlFor="email">Trading API:</label>
               <input
                 type="text"
                 id="tradeAPI"
-                name="tradeAPI"
-                value={formValues.tradeAPI}
+                name="user_api"
+                value={formValues.user_api}
                 onChange={handleInputChange}
               />
             </div>
-            <div class="form-field">
-              <label htmlFor="email">Secret Key:</label>
+            <div className="form-field">
+              <label htmlFor="email">Risk:</label>
               <input
-                type="password"
-                id="tradeAPI"
-                name="tradeAPI"
-                value={formValues.tradeAPI}
+                type="text"
+                id="secretKey"
+                name="user_risk_cap"
+                value={formValues.user_risk_cap}
                 onChange={handleInputChange}
               />
             </div>
 
             <br/>
-            <h2>Trade Preferences</h2>
+            {/* <h2>Trade Preferences</h2>
 
-            <div class="form-field">
-              <label htmlFor="firstName">Risk Level:</label>
+            <div className="form-field">
+              <label htmlFor="riskLevel">Risk Level:</label>
               <input
                 type="text"
-                id="firstName"
-                name="firstName"
-                // value={formValues.firstName}
+                id="riskLevel"
+                name="riskLevel"
+                value={formValues.riskLevel}
                 onChange={handleInputChange}
               />
             </div>
-            <div class="form-field">
-              <label htmlFor="lastName">Market Cap:</label>
+            <div className="form-field">
+              <label htmlFor="marketCap">Market Cap:</label>
               <input
                 type="select"
-                id="lastName"
-                name="lastName"
-                // value={formValues.lastName}
-                onChange={handleInputChange}
-              />
-            </div>
-            {/* <div class="form-field">
-              <label htmlFor="email">Indicator 3:</label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                // value={formValues.email}
+                id="marketCap"
+                name="marketCap"
+                value={formValues.marketCap}
                 onChange={handleInputChange}
               />
             </div> */}
             <br/>
-            <button class="profile-button" type="submit">Save Changes</button>
+            <button className="profile-button" type="submit">Save Changes</button>
           </form>
         </div>
       </div>
